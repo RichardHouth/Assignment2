24674ad514ab810deff87aaa9a16618d5e54e8a2
Richard Houth <rhouth2301@yahoo.com>
2017-07-19 18:31:15 -0700

    Update updateUser.java

--- srcAssignment2/data/updateUser.java
+++ srcAssignment2/data/updateUser.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Assignment2.data;
 import Assignment2.model.GroupComponent;
 import Assignment2.model.User;
