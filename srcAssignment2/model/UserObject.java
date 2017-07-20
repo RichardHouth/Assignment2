4b9c5d2ddb8abb2aa1d5d50f6a0dad10de04c8da
Richard Houth <rhouth2301@yahoo.com>
2017-07-19 19:19:25 -0700

    Update UserObject.java

--- srcAssignment2/model/UserObject.java
+++ srcAssignment2/model/UserObject.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Assignment2.data;
+
 import Assignment2.model.User;
 import Assignment2.model.Tweet;
 import java.util.Set;
@@ -17,7 +13,7 @@ import java.util.List;
  */
 public interface UserObject {
     
-    public static final long FIRST_CREATION_TIME = System.currentTimeMillis();
+    public static final long First_Creation = System.currentTimeMillis();
     public static List<String> usedIDs = new ArrayList<>(Arrays.asList("Root"));
     public String getID();
     public List<Tweet> getMessages();
